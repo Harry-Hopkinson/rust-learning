c1bb2aed917a497ccee654b2b3b742b8d95150aa
Harry Hopkinson <harryjhopkinson@gmail.com>
2023-04-20 09:36:07 +0100

    Methods

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // pass in self as a parameter
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rectangle.area()
+    );
 }
